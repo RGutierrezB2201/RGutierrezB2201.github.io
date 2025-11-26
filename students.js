1f2ee02bfad804d176bf1d80512b92480b69cd98
RGutierrezB2201 <166568132+RGutierrezB2201@users.noreply.github.com>
2025-11-26 20:18:39 -0300

    Create students.js

---
+++ students.js
@@ -0,0 +1,17 @@
+const students = [
+    {
+        name: "Rodrigo (Profesor)",
+        botName: "Cheticus",
+        description: "Un asistente experto en IA y Python enfocado en la enseñanza académica.",
+        link: "https://poe.com/Cheticus",
+        image: "" // Opcional: URL de imagen
+    },
+    {
+        name: "Alumno Ejemplo",
+        botName: "MathBot 3000",
+        description: "Ayuda a resolver integrales y derivadas paso a paso.",
+        link: "https://poe.com/MathBotExample",
+        image: "" 
+    },
+    // Los alumnos agregarán sus bloques aquí abajo
+];
