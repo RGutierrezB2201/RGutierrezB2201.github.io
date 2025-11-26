2e92859bc8fdd7d69bf7ad2e304ce68a05815c99
RGutierrezB2201 <166568132+RGutierrezB2201@users.noreply.github.com>
2025-11-26 20:59:05 -0300

    Update students.js

--- students.js
+++ students.js
@@ -4,14 +4,14 @@ const students = [
         botName: "Asistente del Curso",
         description: "Bot oficial de Chatbase para demostración.",
         // OJO: Aquí va la URL del iframe, NO la del sitio web
-        embedUrl: "https://www.chatbase.co/embed.min.js%22;script.id=%225j23HcpRyfT6De7B5GT0_%22;script.domain=%22www.chatbase.co%22;document.body.appendChild(script)%7D;if(document.readyState===%22complete%22)%7BonLoad()%7Delse%7Bwindow.addEventListener(%22load%22,onLoad)%7D%7D)();", 
+        embedUrl: "https://chatbase.co/", 
         image: "" 
     },
     {
         name: "Alumno Ejemplo",
         botName: "Tutor de Python",
         description: "Ayuda con sintaxis básica.",
-        embedUrl: "https://www.chatbase.co/chatbot-iframe/ID_DEL_ALUMNO",
+        embedUrl: "https://www.chatbase.co/chatbot-iframe/5j23HcpRyfT6De7B5GT0_"
         image: "" 
     },
     // Nuevos alumnos aquí...
